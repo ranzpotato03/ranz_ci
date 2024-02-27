82342d171c2faf8a26b2bf0f18111423559fdf5b
Ranz <ranzkurtle02@gmail.com>
2024-02-27 12:10:08 +0800

    intl commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'a_ci_cb',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
+use App\Models\GenderModels;
+
 class UserController extends BaseController
 {
     public function index(): string
@@ -9,7 +12,58 @@ class UserController extends BaseController
         return view('welcome_message');
     }
 
-    public function adduser(){
-        return view('user/add');
+    public function adduser()
+    {
+        $data = array();
+        helper('form');
+
+        //When button is clicked
+        if($this->request == 'post')
+        {
+            $post = $this->request->getPost(['first_name', 'middle_name',
+             'last_name', 'age', 'gender_id',
+            'email', 'password']);
+
+            //Provide
+             $rules = [
+                'first_name' => ['label' => 'first_name',
+                'rules' => 'required'], 
+                'middle_name' => ['label' => 'middle_name',
+                'rules' => 'permit_empty'],
+                'last_name' => ['label' => 'last_name',
+                'rules' => 'required'],
+                'age' => ['label' => 'age',
+                'rules' => 'required|numeric'],
+                'gender_id' => ['required'],
+                'email' => ['label' => 'email',
+                'rules' => 'required|is_unique[users.email]'],
+                'password' => ['label' => 'password',
+                'rules' => 'required'],
+                'confirm_password' => ['label' => 'confirm password',
+                'rules' => 'required_with[password]']
+             ];
+
+             if(! $this->validate ($rules))
+             {
+                $data['validation'] = $this->validator;
+             } 
+             
+             else 
+             {
+                // Encrypt Password
+                $post['password'] = sha1($post['password']);
+
+                $userModel = new UserModel();
+                $userModel ->save($post);
+
+                return 'User Successfully saved!';
+             }
+        }
+
+        // Fetch all values from genders table
+        $genderModel = new GenderModel();
+        $data = [genders] = $genderModel->findAll();
+
+        return view('user/add', $data);
     }
 }
